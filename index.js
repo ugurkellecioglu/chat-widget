8cc82b86e02278f3fb5b2da4f5f571cad39e2cef
Ugur <ugur.kellecioglu@outlook.com>
2024-02-22 21:58:04 +0300

    d

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const url = new URL(import.meta.url)
-
-console.log("url", url)
-
-const modelHash = url.searchParams.get("modelHash")
-
+const urlParams = new URLSearchParams(new URL(import.meta.url))
+const modelHash = urlParams.get("modelHash")
 console.log("modelHash", modelHash)
-
-import(`./main-odPP3p5S.js?modelHash=${modelHash}`)
+import(`./main-PLonMSs9.js?modelHash=${modelHash})`)
