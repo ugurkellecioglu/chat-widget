2644236c68817fb40a4b2d25edebd507daaf9d9f
Ugur <ugur.kellecioglu@outlook.com>
2024-02-22 21:29:47 +0300

    b

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-import "./main-13_DRmHc.js";
+const urlParams = new URLSearchParams(new URL(import.meta.url))
+const modelHash = urlParams.get("modelHash")
+import(`./main-13_DRmHc.js?modelHash=${modelHash})`)
