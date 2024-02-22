91bb9d147a676a5935733a6d2816cef95371099a
Ugur <ugur.kellecioglu@outlook.com>
2024-02-22 22:21:08 +0300

    d

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-import "./main-PQrmMtBH.js";
+// import "./main-PQrmMtBH.js";
+const url = new URL(import.meta.url)
+const modelHash = url.searchParams.get("modelHash")
+import(`./main-PQrmMtBH.js?modelHash=${modelHash}`)
